4006f8ce542dab50a808027fb91226ebea09a763
Abdullah <amohammad2002@outlook.com>
2021-06-25 21:25:22 +0530

    Save screenshot to device implemented for API 29 & 30.

--- app/src/main/java/com/example/birthdayapplication/birthday.java
+++ app/src/main/java/com/example/birthdayapplication/birthday.java
@@ -2,15 +2,18 @@ package com.example.birthdayapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
-import androidx.core.content.FileProvider;
 
 import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
@@ -20,7 +23,8 @@ import android.widget.Toast;
 
 import java.io.File;
 import java.io.FileOutputStream;
-import java.util.Calendar;
+import java.io.OutputStream;
+import java.util.Objects;
 
 import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
 import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
@@ -67,40 +71,55 @@ public class birthday extends AppCompatActivity {
         });
     }
 
-    public File takeScreenshot(View view, String fileName) {
+    public void takeScreenshot(View view, String fileName) {
         try {
-            String dirPath = Environment.getExternalStorageDirectory() + "/DCIM/Screenshots";
-            File file = new File(dirPath);
-            if (!file.exists())
-                file.mkdir();
-
-            String filePath = dirPath + "/" + fileName + " - " + Calendar.getInstance().getTime().toString() + ".jpeg";
+//            String dirPath = Environment.getExternalStorageDirectory() + "/DCIM/Screenshots";
+//            File file = new File(dirPath);
+//            if (!file.exists())
+//                file.mkdir();
+//
+//            String filePath = dirPath + "/" + fileName + " - " + Calendar.getInstance().getTime().toString() + ".jpeg";
 
             view.setDrawingCacheEnabled(true);
             Bitmap bitmap = Bitmap.createBitmap(view.getDrawingCache());
             view.setDrawingCacheEnabled(false);
 
-            File imageurl = new File(filePath);
-            FileOutputStream fos = new FileOutputStream(imageurl);
-            bitmap.compress(Bitmap.CompressFormat.JPEG, 85, fos);
-            fos.flush();
-            fos.close();
+//            File imageurl = new File(filePath);
 
-            Intent intent = new Intent(Intent.ACTION_SEND);
-            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            Uri uri = FileProvider.getUriForFile(getApplicationContext(), BuildConfig.APPLICATION_ID + ".provider", imageurl);
-            intent.putExtra(Intent.EXTRA_STREAM,uri);
-            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-            intent.setType("image/jpeg");
+            OutputStream fos;
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                ContentResolver resolver = getContentResolver();
+                ContentValues values = new ContentValues();
+                values.put(MediaStore.MediaColumns.DISPLAY_NAME, "Image " + System.currentTimeMillis() + ".jpg");
+                values.put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg");
+                values.put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_PICTURES + File.separator + "TestFolder");
+                Uri imageUrl = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+                fos = (FileOutputStream) resolver.openOutputStream(Objects.requireNonNull(imageUrl));
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+                Objects.requireNonNull(fos);
 
-            startActivity(Intent.createChooser(intent,"Open via"));
+                Toast.makeText(this, "Image Saved", Toast.LENGTH_SHORT).show();
 
-            return imageurl;
+//                bitmap.compress(Bitmap.CompressFormat.JPEG, 85, fos);
+//                fos.flush();
+//                fos.close();
+            }
+
+//            Intent intent = new Intent(Intent.ACTION_SEND);
+//            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//            Uri uri = FileProvider.getUriForFile(getApplicationContext(), BuildConfig.APPLICATION_ID + ".provider", imageurl);
+//            intent.putExtra(Intent.EXTRA_STREAM, uri);
+//            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+//            intent.setType("image/jpeg");
+//
+//            startActivity(Intent.createChooser(intent, "Open via"));
+
+//            return imageurl;
         } catch (Exception e) {
             Toast.makeText(this, "Something went wrong.", Toast.LENGTH_SHORT).show();
         }
 
-        return null;
+//        return null;
     }
 
     public static void verifystoragepermissions(Activity activity) {
